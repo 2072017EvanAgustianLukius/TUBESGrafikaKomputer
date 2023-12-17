4e4cc1c30c69f541e3a903f836f06cd6b0f7a054
2072017EvanAgustianLukius <71422920+2072017EvanAgustianLukius@users.noreply.github.com>
2023-12-17 10:33:10 +0700

    Update 17-12-2023

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import { OrbitControls } from '../node_modules/three/examples/jsm/controls/Orbit
 import { GLTFLoader } from '../node_modules/three/examples/jsm/loaders/GLTFLoader.js';
 
 const scene = new THREE.Scene();
-const cam = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const cam = new THREE.PerspectiveCamera(100, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
 scene.background = new THREE.Color(0xaaaaaa);
 renderer.shadowMap.enabled = true;
@@ -15,17 +15,17 @@ document.body.appendChild(renderer.domElement);
 
 const controls = new OrbitControls(cam, renderer.domElement);
 
-const loader = new GLTFLoader().setPath('../models/');
-loader.load('Laptop_bag.gltf', (gltf) => {
+const loader = new GLTFLoader().setPath('../models/mp3_player_free/');
+loader.load('scene.gltf', (gltf) => {
     const laptop = gltf.scene;
     laptop.position.set(0, 0, 0);
-    laptop.scale.set(100, 100, 100);
+    laptop.scale.set(1, 1, 1);
     scene.add(laptop);
     console.log(laptop);
 });
 
-const light = new THREE.PointLight(0xff0000, 2, 100);
-light.position.set(-2, 2, 2);
+const light = new THREE.PointLight(0xffffff, 200, 100);
+light.position.set(0, 10, 0);
 scene.add(light);
 scene.add(new THREE.PointLightHelper(light, 0.5, 0x00ff00));
 
