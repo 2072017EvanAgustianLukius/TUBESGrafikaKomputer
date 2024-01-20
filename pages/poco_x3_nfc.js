dc77293c37ba106a303b4193dc27abaa21d14af2
2172016AlvinRahmatilasyah <90677192+2172016AlvinRahmatilasyah@users.noreply.github.com>
2024-01-20 18:34:12 +0700

    update

--- pages/poco_x3_nfc.js
+++ pages/poco_x3_nfc.js
@@ -21,13 +21,18 @@ document.body.appendChild(renderer.domElement);
 const controls = new OrbitControls(cam, renderer.domElement);
 
 const loader = new GLTFLoader().setPath('../models/poco_x3_nfc/');
+let consolps;
+const loadingOverlay = document.getElementById('loading-overlay');
+
 loader.load('scene.gltf', (gltf) => {
-    const consolps = gltf.scene;
+     consolps = gltf.scene;
     consolps.position.set(0, 0, 0);
     consolps.scale.set(1, 1, 1);
     consolps.rotation.x += 0.01;
     scene.add(consolps);
     console.log(consolps);
+
+    loadingOverlay.style.display = 'none';
 });
 
 // new RGBELoader()
